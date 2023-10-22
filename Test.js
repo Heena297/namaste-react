8b83d0df7f642a47fe48d70b17936e845d178d99
Heena297 <jindalheena14@gmail.com>
2023-10-22 11:44:16 +0530

    insert code in git

---
+++ Test.js
@@ -0,0 +1,18 @@
+ const parenting = React.createElement("div", {id : "parent"},
+ [React.createElement("div", {id : "child"},
+ [React.createElement("h1", {}, "I'm an h1 tag"),
+ React.createElement("h1", {}, "I'm an h2 tag")
+ ]
+ ),
+ React.createElement("div", {id : "child"},
+ [React.createElement("h1", {}, "I'm an h1 tag"),
+ React.createElement("h1", {}, "I'm an h2 tag")
+ ]
+ )
+ ]
+ );
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parenting);
+ 
+
